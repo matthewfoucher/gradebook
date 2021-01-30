42afd1ad0c7c9cc47c91a6d94c0a19749740edac
matthewfoucher <matthewfoucher@gmail.com>
2021-01-29 19:28:10 -0800

    Finished Module 7.

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -14,14 +14,37 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            if (grade < 0 || grade > 100)
+            if (grade <= 100 && grade >= 0)
             {
-
+                grades.Add(grade);
             }
-
-            grades.Add(grade);
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -30,13 +53,33 @@ namespace GradeBook
             result.Average = 0.0;
             result.High = double.MinValue;
             result.Low = double.MaxValue;
-            foreach (var grade in grades)
+            
+            for (var i = 0; i < grades.Count; i++)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                result.Low = Math.Min(grades[i], result.Low);
+                result.High = Math.Max(grades[i], result.High);
+                result.Average += grades[i];
             }
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
